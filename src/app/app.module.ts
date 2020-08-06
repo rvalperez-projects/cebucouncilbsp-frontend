08083b82ba26253bfe3202bb7c9f355c1fa349c8
reneir.val.t.perez <reneir.val.t.perez@accenture.com>
2020-08-06 22:05:24 +0800

    [develop] Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { FormsListComponent } from './forms-list/forms-list.component';
+import { FormRegistrationComponent } from './form-registration/form-registration.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { PaymentComponent } from './payment/payment.component';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProfileComponent,
+    FormsListComponent,
+    FormRegistrationComponent,
+    UsersListComponent,
+    PaymentComponent,
+    LeftMenuComponent,
+    TopMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/form-registration/form-registration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-registration',
+  templateUrl: './form-registration.component.html',
+  styleUrls: ['./form-registration.component.css']
+})
+export class FormRegistrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
