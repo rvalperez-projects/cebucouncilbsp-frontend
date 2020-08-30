441669f8e49ec9385241b65491673c595d15ac7c
reneir.val.t.perez <reneir.val.t.perez@accenture.com>
2020-08-30 23:02:51 +0800

    [develop] Re-organized imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogModule } from '@angular/material/dialog';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AngularMaterialModule } from './angular-material.module';
-
+import { ErrorDialog } from './component/common-components/dialog/error-dialog.component';
+import { HomeComponent } from './component/common-components/home/home.component';
 // Common Components
 import { LoginComponent } from './component/common-components/login/login.component';
 import { PaymentComponent } from './component/common-components/payment/payment.component';
-import { ErrorDialog } from './component/common-components/dialog/error-dialog.component';
-import { HomeComponent } from './component/common-components/home/home.component';
-
-// User Components
-import { ProfileComponent } from './component/user/profile/profile.component';
-import { UsersListComponent } from './component/user/users-list/users-list.component';
-import { SignUpComponent } from './component/user/sign-up/sign-up.component';
-
+import { AurFormUpdateComponent } from './component/forms-aur/aur-form-update/aur-form-update.component';
+import { AurFormViewComponent } from './component/forms-aur/aur-form-view/aur-form-view.component';
+import { FormRegistrationComponent } from './component/forms-aur/form-registration/form-registration.component';
 // Forms AUR
 import { FormsListComponent } from './component/forms-aur/forms-list/forms-list.component';
-import { FormRegistrationComponent } from './component/forms-aur/form-registration/form-registration.component';
-import { AurFormViewComponent } from './component/forms-aur/aur-form-view/aur-form-view.component';
-import { AurFormUpdateComponent } from './component/forms-aur/aur-form-update/aur-form-update.component';
-
+// User Components
+import { ProfileComponent } from './component/user/profile/profile.component';
+import { SignUpComponent } from './component/user/sign-up/sign-up.component';
+import { UsersListComponent } from './component/user/users-list/users-list.component';
+import { HttpErrorInterceptor } from './utils/http-interceptor.utils';
 // Utils Components
 import { MatSpinnerOverlayComponent } from './utils/mat-spinner-overlay/mat-spinner-overlay.component';
-import { HttpErrorInterceptor } from './utils/http-interceptor.utils';
+
+
+
+
+
 
 
 @NgModule({
