9d22511a7ca6c68e9be9fb66c04bf86ce68e1c5e
rvalperez-projects <68724645+rvalperez-projects@users.noreply.github.com>
2020-10-01 01:01:19 +0800

    Revert "[developed] Backend moved to AWS"

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: "http://cebucouncilbspbackend-env.eba-pyu82p33.ap-southeast-1.elasticbeanstalk.com"
+  apiUrl: "https://cebucouncilbsp.herokuapp.com"
 };
